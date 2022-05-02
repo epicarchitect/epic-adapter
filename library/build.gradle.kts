9427fff097291280d2c6f6be9f946548e08e9fab
Alexander Kolmachikhin <alexanderkolmachikhin@gmail.com>
2022-05-02 10:26:17 +0700

    save

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -52,7 +52,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.alexander-alexander"
             artifactId = "BindingRecyclerViewAdapter"
-            version = "1.0.1"
+            version = "1.0.2"
             afterEvaluate {
                 from(components["release"])
             }
